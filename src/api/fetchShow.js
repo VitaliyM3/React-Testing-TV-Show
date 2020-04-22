e51c4858bdb427e3011823711eea42849b8f8712
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2020-04-22 16:56:55 -0400

    refactored code the way repo requested

---
+++ src/api/fetchShow.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const fetchShow = () => {
+    return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
+    .then(res => res)
+};
+
+
+
+// useEffect(() => {
+//   const fetchShow = () => {
+//     axios
+//       .get(
+//         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+//       )
+//       .then(res => {
+//         setShow(res.data);
+//         setSeasons(formatSeasons(res.data._embedded.episodes));
+//       });
+//   };
+//   fetchShow();
+// }, []);
